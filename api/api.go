3beaabb88c7b85849a2bb2f58d8677ab98a38151
Richard Goater <rg19@sanger.ac.uk>
2022-01-13 00:04:43 +0000

    [wip] flexible callback style, query param validation

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"regexp"
 	"sort"
 	"strings"
 	"time"
@@ -12,54 +13,93 @@ import (
 )
 
 type Opts struct {
-	DataFile   string
-	InfoFile   string
-	PathPrefix string
+	InfoFile    string
+	PathPrefix  string
+	MaxLineages int
 }
 
-func parseQuery(qs map[string][]string) covince.Query {
-	var q covince.Query
-	if _l, ok := qs["lineages"]; ok {
-		lineages := strings.Split(_l[0], ",")
-		sort.Sort(sort.Reverse(sort.StringSlice(lineages))) // TODO: implement real sort
-		parsedLineages := make([]covince.QueryLineage, len(lineages))
-		for i, v := range lineages {
-			split := strings.Split(v, "+")
-			parsedLineages[i] = covince.QueryLineage{
+var isPangoLineage = regexp.MustCompile(`^[A-Z]{1,3}(\.[0-9]+)*$`)
+var isDateString = regexp.MustCompile(`^[0-9]{4}-[0-9]{2}-[0-9]{2}$`)
+
+func parseLineages(lineages []string) ([]covince.QueryLineage, error) {
+	index := make(map[string]covince.QueryLineage)
+	for _, v := range lineages {
+		split := strings.Split(v, "+")
+		lineage := split[0]
+		mutations := split[1:] // TODO: first two mutations only
+		if !isPangoLineage.MatchString(split[0]) {
+			return nil, fmt.Errorf("invalid lineages")
+		}
+		if _, ok := index[v]; !ok {
+			index[v] = covince.QueryLineage{
 				Key:        v,
-				PangoClade: split[0],
-				Mutations:  split[1:],
+				PangoClade: lineage + ".",
+				Mutations:  mutations,
 			}
 		}
-		q.Lineages = parsedLineages
+		if _, ok := index[lineage]; len(mutations) > 0 && !ok {
+			index[lineage] = covince.QueryLineage{
+				Key:        lineage,
+				PangoClade: lineage + ".",
+			}
+		}
 	}
-	if _a, ok := qs["area"]; ok {
-		q.Area = _a[0]
+	keys := make([]string, len(index))
+	i := 0
+	for k := range index {
+		keys[i] = k
+		i++
 	}
-	fmt.Println(q, qs)
-	return q
+	sort.Sort(sort.Reverse(sort.StringSlice(keys))) // TODO: implement real sort
+	parsedLineages := make([]covince.QueryLineage, len(keys))
+	for i, k := range keys {
+		parsedLineages[i] = index[k]
+	}
+	return parsedLineages, nil
 }
 
-func CovinceAPI(opts Opts, s []covince.Record) {
-	http.HandleFunc(opts.PathPrefix+"/frequency", func(rw http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			rw.WriteHeader(http.StatusMethodNotAllowed)
-			return
+func parseQuery(qs map[string][]string, maxLineages int) (covince.Query, error) {
+	var q covince.Query
+	if lineages, ok := qs["lineages"]; ok {
+		lineages = strings.Split(lineages[0], ",")
+		if len(lineages) > maxLineages {
+			return q, fmt.Errorf("too many lineages")
 		}
-		qs := r.URL.Query()
-		q := parseQuery(qs)
-
-		i := make(covince.Index)
-		for _, r := range s {
-			covince.Frequency(i, q, r)
+		p, err := parseLineages(lineages)
+		if err != nil {
+			return q, err
 		}
+		q.Lineages = p
+	}
+	if a, ok := qs["area"]; ok {
+		q.Area = a[0]
+	}
+	if from, ok := qs["from"]; ok {
+		if !isDateString.MatchString(from[0]) {
+			return q, fmt.Errorf("invalid date")
+		}
+		q.DateFrom = from[0]
+	}
+	if to, ok := qs["to"]; ok {
+		if !isDateString.MatchString(to[0]) {
+			return q, fmt.Errorf("invalid date")
+		}
+		q.DateTo = to[0]
+	}
+	if excluding, ok := qs["excluding"]; ok {
+		excluding = strings.Split(excluding[0], ",")
+		for _, lineage := range excluding {
+			if !isPangoLineage.MatchString(lineage) {
+				return q, fmt.Errorf("invalid lineages")
+			}
+		}
+	}
+	fmt.Println(q, qs)
+	return q, nil
+}
 
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		json.NewEncoder(rw).Encode(i)
-	})
-
-	http.HandleFunc(opts.PathPrefix+"/spatiotemporal/total", func(rw http.ResponseWriter, r *http.Request) {
+func CovinceAPI(opts Opts, foreach func(func(r covince.Record))) {
+	http.HandleFunc(opts.PathPrefix+"/", func(rw http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
 		if r.Method != "GET" {
@@ -67,62 +107,46 @@ func CovinceAPI(opts Opts, s []covince.Record) {
 			return
 		}
 		qs := r.URL.Query()
-		q := parseQuery(qs)
-
-		i := make(covince.Index)
-		for _, r := range s {
-			covince.Totals(i, q, r)
-		}
-
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		json.NewEncoder(rw).Encode(i)
-
-		duration := time.Since(start)
-		fmt.Println(duration.Milliseconds(), "ms")
-	})
-
-	http.HandleFunc(opts.PathPrefix+"/spatiotemporal/lineage", func(rw http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		if r.Method != "GET" {
-			rw.WriteHeader(http.StatusMethodNotAllowed)
+		q, err := parseQuery(qs, opts.MaxLineages)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadRequest)
 			return
 		}
-		qs := r.URL.Query()
-		q := parseQuery(qs)
 
-		i := make(covince.Index)
-		for _, r := range s {
-			covince.Spatiotemporal(i, q, r)
+		var response interface{}
+
+		if r.URL.Path == opts.PathPrefix+"/frequency" {
+			i := make(covince.Index)
+			foreach(func(r covince.Record) {
+				covince.Frequency(i, q, r)
+			})
+			response = i
+		}
+		if r.URL.Path == opts.PathPrefix+"/spatiotemporal/total" {
+			i := make(covince.Index)
+			foreach(func(r covince.Record) {
+				covince.Totals(i, q, r)
+			})
+			response = i
+		}
+		if r.URL.Path == opts.PathPrefix+"/spatiotemporal/lineage" {
+			i := make(covince.Index)
+			foreach(func(r covince.Record) {
+				covince.Spatiotemporal(i, q, r)
+			})
+			response = i
+		}
+		if r.URL.Path == opts.PathPrefix+"/lineages" {
+			m := make(map[string]int)
+			foreach(func(r covince.Record) {
+				covince.Lineages(m, q, r)
+			})
+			response = m
 		}
 
 		rw.Header().Set("Content-Type", "application/json")
 		rw.WriteHeader(http.StatusOK)
-		json.NewEncoder(rw).Encode(i)
-
-		duration := time.Since(start)
-		fmt.Println(duration.Milliseconds(), "ms")
-	})
-
-	http.HandleFunc(opts.PathPrefix+"/lineages", func(rw http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		if r.Method != "GET" {
-			rw.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-		qs := r.URL.Query()
-		q := parseQuery(qs)
-
-		m := make(map[string]int)
-		for _, r := range s {
-			covince.Lineages(m, q, r)
-		}
-
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		json.NewEncoder(rw).Encode(m)
+		json.NewEncoder(rw).Encode(response)
 
 		duration := time.Since(start)
 		fmt.Println(duration.Milliseconds(), "ms")
--- server.go
+++ server.go
@@ -12,35 +12,70 @@ import (
 	"github.com/covince/covince-backend-v2/covince"
 )
 
-func readData(filePath string) []covince.Record {
+func createRecordFromCsv(row []string) covince.Record {
+	count, _ := strconv.Atoi(row[4])
+	return covince.Record{
+		Date:       row[0],
+		PangoClade: row[1],
+		Area:       row[2],
+		Mutations:  row[3],
+		Count:      count,
+	}
+}
+
+func server(filePath string) {
 	csvfile, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
 	}
 
 	scanner := bufio.NewScanner(csvfile)
-
 	s := make([]covince.Record, 0)
 
 	for scanner.Scan() {
 		row := strings.Split(scanner.Text(), ",")
-		count, _ := strconv.Atoi(row[4])
-		r := covince.Record{
-			Date:       row[0],
-			PangoClade: row[1],
-			Area:       row[2],
-			Mutations:  row[3],
-			Count:      count,
-		}
+		r := createRecordFromCsv(row)
 		s = append(s, r)
 	}
+	log.Println(len(s), "records")
 
-	return s
+	api.CovinceAPI(api.Opts{MaxLineages: 16}, func(agg func(r covince.Record)) {
+		for _, r := range s {
+			agg(r)
+		}
+	})
+}
+
+func serverless(filePath string) {
+	api.CovinceAPI(api.Opts{MaxLineages: 16}, func(agg func(r covince.Record)) {
+		csvfile, err := os.Open(filePath)
+		// csvfile, err := os.Open("input2.tsv")
+		if err != nil {
+			log.Fatalln("Couldn't open the csv file", err)
+		}
+
+		c := make(chan covince.Record, 500)
+		done := make(chan bool)
+		go func() {
+			for r := range c {
+				agg(r)
+			}
+			done <- true
+		}()
+
+		scanner := bufio.NewScanner(csvfile)
+		for scanner.Scan() {
+			row := strings.Split(scanner.Text(), ",")
+			c <- createRecordFromCsv(row)
+		}
+		close(c)
+		<-done
+	})
 }
 
 func main() {
-	s := readData("aggregated.csv")
-	log.Println(len(s), "records")
-	api.CovinceAPI(api.Opts{}, s)
+	server("aggregated.csv")
+	// serverless("aggregated.csv")
+
 	http.ListenAndServe(":4000", nil)
 }
